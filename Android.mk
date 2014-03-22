338ab0dcebd945cef467a2aac4c2b983b284a76e
Emil <suleymanovemil8@gmail.com>
2014-03-22 23:46:37 +0400

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,10 +2,13 @@ LOCAL_PATH := $(call my-dir)
  
 include $(CLEAR_VARS)
  
-LOCAL_MODULE    := libjni_sonyopenir
 LOCAL_SRC_FILES := irjni.c \
                    libsonyir.c
 
-LOCAL_SHARED_LIBRARIES := liblog
-LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libnativehelper \
+
+LOCAL_MODULE := libjni_sonyopenir
+
 include $(BUILD_SHARED_LIBRARY)
